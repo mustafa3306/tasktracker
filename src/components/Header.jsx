6c4f44b8c0d6a03d6f0ea09c5bc30a8c4c5ae50d
mustafa3306 <mustafaworkkok@gmail.com>
2022-07-01 15:23:07 +0300

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './Button'
+
+const Header = () => {
+  return (
+    <Button />
+  )
+}
+
+export default Header
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Task from './Task'
 
 export const Tasks = () => {
   return (
-    <div>Tasks</div>
+    <Task />
   )
 }
